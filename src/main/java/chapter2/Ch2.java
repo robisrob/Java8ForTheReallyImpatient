591e0abcaa2f13c8de4729fb0655c3097835846f
Sch3lp <sch3lp@gmail.com>
2015-09-20 17:17:45 +0200

    ch2 - exercise2

--- src/main/java/chapter2/Ch2.java
+++ src/main/java/chapter2/Ch2.java
@@ -1,5 +1,6 @@
 package chapter2;
 
+import java.util.concurrent.locks.ReentrantLock;
 import java.util.function.Supplier;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -9,11 +10,24 @@ import java.util.logging.Logger;
  */
 public class Ch2 {
 
-    static Logger logger = Logger.getAnonymousLogger();
+    private Logger logger = null;
 
-    public static void logIf(Level level, Supplier<Boolean> extraCondition, Supplier<String> log){
+    public Ch2(Logger logger) {
+        this.logger = logger;
+    }
+
+    public void logIf(Level level, Supplier<Boolean> extraCondition, Supplier<String> log){
         if (extraCondition.get()) {
             logger.log(level, log);
+        }
+    }
+
+    public static void withLock(ReentrantLock reentrantLock, Supplier<String> action){
+        reentrantLock.lock();
+        try {
+            action.get();
+        } finally {
+            reentrantLock.unlock();
         }
     }
 }
--- src/test/java/chapter2/Ch2Test.java
+++ src/test/java/chapter2/Ch2Test.java
@@ -1,10 +1,16 @@
 package chapter2;
 
+import org.assertj.core.api.Assertions;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.locks.ReentrantLock;
 import java.util.logging.Level;
+import java.util.logging.Logger;
 
-import static org.assertj.core.api.StrictAssertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 public class Ch2Test {
 
@@ -12,16 +18,39 @@ public class Ch2Test {
     public void logIf_OnlyLogsIfLevelIsAppropriate() throws Exception {
         final boolean[] fineWasLogged = {false};
         final boolean[] infoWasLogged = {false};
-        Ch2.logger.setLevel(Level.INFO);
-        Ch2.logIf(Level.INFO, () -> false, () -> {
+        Logger ch2Logger = Logger.getLogger("Ch2Logger");
+        ch2Logger.setLevel(Level.INFO);
+        Ch2 ch2 = new Ch2(ch2Logger);
+        ch2.logIf(Level.INFO, () -> false, () -> {
             fineWasLogged[0] = true;
             return "fine should not be logged";
         });
-        Ch2.logIf(Level.INFO, () -> true, () -> {
+        ch2.logIf(Level.INFO, () -> true, () -> {
             infoWasLogged[0] = true;
             return "info should be logged";
         });
         assertThat(fineWasLogged[0]).isFalse();
         assertThat(infoWasLogged[0]).isTrue();
+    }
+
+    @Test
+    public void withLock_WrapsAnActionWithLockUnlock() throws Exception {
+        List<String> verifyActions = new ArrayList<>();
+        ReentrantLock myLock = new ReentrantLock() {
+            @Override
+            public void lock() {
+                verifyActions.add("lockCalled");
+            }
+
+            @Override
+            public void unlock() {
+                verifyActions.add("unlockCalled");
+            }
+        };
+        Ch2.withLock(myLock, () -> {
+            verifyActions.add("doing some private business");
+            return "doing some private business";
+        });
+        assertThat(verifyActions).containsExactly("lockCalled", "doing some private business", "unlockCalled");
     }
 }
