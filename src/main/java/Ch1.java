b3d05400e2eb451cb2eafe86eea5bf9de5586177
Sch3lp <sch3lp@gmail.com>
2015-09-20 11:18:41 +0200

    Ch1

---
+++ src/main/java/Ch1.java
@@ -0,0 +1,19 @@
+/**
+ * Created by sch3lp on 20/09/15.
+ */
+public class Ch1 {
+
+    private String bar;
+
+    public Ch1() {
+    }
+
+    public void cocktail(String bar){
+        this.bar = bar;
+    }
+
+    public String getBar() {
+        return bar;
+    }
+
+}
---
+++ src/test/java/Ch1Test.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+import static org.assertj.core.api.StrictAssertions.assertThat;
+
+public class Ch1Test {
+
+    @Test
+    public void cocktail_ShouldSetBar() throws Exception {
+        Ch1 ch1 = new Ch1();
+        ch1.cocktail("bar");
+
+        assertThat(ch1.getBar()).isEqualTo("bar");
+    }
+}
