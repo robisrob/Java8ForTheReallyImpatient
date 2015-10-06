ab6ade21ab6ece98c8b9c60e565f02dfda62abec
Sch3lp <sch3lp@gmail.com>
2015-10-06 20:55:13 +0200

    added infinite streams that are limited

--- src/test/java/chapter2/Ch2Test.java
+++ src/test/java/chapter2/Ch2Test.java
@@ -32,4 +32,20 @@ public class Ch2Test {
 
         assertThat(Ch2.zip(stream1, stream2).collect(Collectors.toList())).containsExactly("Zip1", "Zop1", "Zip2", "Zop2", "Zip3", "Zop3");
     }
+
+    @Test
+    public void ex8_zip_FirstStreamRunsOut_ReturnsAZippedStreamWithLastItemOfFirstStream() throws Exception {
+        Stream<Integer> stream1 = Stream.iterate(1, zip -> ++zip);
+        Stream<Integer> stream2 = Stream.iterate(1, zop -> ++zop);
+        Stream<Integer> actual = Ch2.zip(stream1.limit(3), stream2.limit(5));
+        assertThat(actual.collect(Collectors.toList())).containsExactly(1,1,2,2,3,3);
+    }
+
+    @Test
+    public void ex8_zip_SecondStreamRunsOut_ReturnsAZippedStreamWithLastItemOfSecondStream() throws Exception {
+        Stream<Integer> stream1 = Stream.iterate(1, zip -> ++zip);
+        Stream<Integer> stream2 = Stream.iterate(1, zop -> ++zop);
+        Stream<Integer> actual = Ch2.zip(stream1.limit(6), stream2.limit(5));
+        assertThat(actual.collect(Collectors.toList())).containsExactly(1,1,2,2,3,3,4,4,5,5);
+    }
 }
