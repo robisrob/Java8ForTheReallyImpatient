58f71a244111857220abf6063edb836c9d2166ee
Sch3lp <sch3lp@gmail.com>
2015-10-06 22:00:31 +0200

    exercise 9

--- src/main/java/chapter2/Ch2.java
+++ src/main/java/chapter2/Ch2.java
@@ -2,10 +2,14 @@ package chapter2;
 
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.function.BiFunction;
 import java.util.stream.Stream;
 
 public class Ch2 {
 
+    /**
+     * Exercise 8
+     */
     public static <T> Stream<T> zip(Stream<T> first, Stream<T> second) {
         Iterator<T> firstIter = first.iterator();
         Iterator<T> secondIter = second.iterator();
@@ -18,5 +22,35 @@ public class Ch2 {
             result.add(secondIter.next());
         }
         return result.stream();
+    }
+
+    /**
+     * Exercise 9
+     */
+    public static ArrayList<String> reduceList_1(Stream<ArrayList<String>> input) {
+        return input.reduce((prev, cur)-> {
+            prev.addAll(cur);
+            return prev;
+        }).orElse(new ArrayList<>());
+    }
+
+    public static ArrayList<String> reduceList_2(Stream<ArrayList<String>> input) {
+        return input.reduce(new ArrayList<>(),
+                (prev, cur)-> {
+            prev.addAll(cur);
+            return prev;
+        });
+    }
+
+    public static ArrayList<String> reduceList_3(Stream<ArrayList<String>> input) {
+        return input.reduce(new ArrayList<String>(),
+                (initialArray, oneOfTheArrayLists) -> {
+                    initialArray.addAll(oneOfTheArrayLists);
+                    return initialArray;
+                },
+                (prev, cur)-> {
+                    prev.addAll(cur);
+                    return prev;
+                });
     }
 }
--- src/test/java/chapter2/Ch2Test.java
+++ src/test/java/chapter2/Ch2Test.java
@@ -2,9 +2,11 @@ package chapter2;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import static java.util.stream.Collectors.toCollection;
 import static org.assertj.core.api.Assertions.*;
 
 public class Ch2Test {
@@ -45,7 +47,38 @@ public class Ch2Test {
     public void ex8_zip_SecondStreamRunsOut_ReturnsAZippedStreamWithLastItemOfSecondStream() throws Exception {
         Stream<Integer> stream1 = Stream.iterate(1, zip -> ++zip);
         Stream<Integer> stream2 = Stream.iterate(1, zop -> ++zop);
-        Stream<Integer> actual = Ch2.zip(stream1.limit(6), stream2.limit(5));
+        Stream<Integer> actual = Ch2.zip(stream1.limit(7), stream2.limit(5));
         assertThat(actual.collect(Collectors.toList())).containsExactly(1,1,2,2,3,3,4,4,5,5);
     }
+
+    @Test
+    public void ex9_reduce_1() throws Exception {
+        ArrayList<String> arrayList1 = Stream.of("String1", "String2").collect(toCollection(ArrayList::new));
+        ArrayList<String> arrayList2 = Stream.of("Strong3", "Strong4").collect(toCollection(ArrayList::new));
+        Stream<ArrayList<String>> stream = Stream.of(arrayList1, arrayList2);
+
+        ArrayList<String> actual1 = Ch2.reduceList_1(stream);
+        assertThat(actual1).containsExactly("String1", "String2", "Strong3", "Strong4");
+    }
+
+    @Test
+    public void ex9_reduce_2() throws Exception {
+        ArrayList<String> arrayList1 = Stream.of("String1", "String2").collect(toCollection(ArrayList::new));
+        ArrayList<String> arrayList2 = Stream.of("Strong3", "Strong4").collect(toCollection(ArrayList::new));
+        Stream<ArrayList<String>> stream = Stream.of(arrayList1, arrayList2);
+
+        ArrayList<String> actual1 = Ch2.reduceList_2(stream);
+        assertThat(actual1).containsExactly("String1", "String2", "Strong3", "Strong4");
+    }
+
+    @Test
+    public void ex9_reduce_3() throws Exception {
+        ArrayList<String> arrayList1 = Stream.of("String1", "String2").collect(toCollection(ArrayList::new));
+        ArrayList<String> arrayList2 = Stream.of("Strong3", "Strong4").collect(toCollection(ArrayList::new));
+        Stream<ArrayList<String>> stream = Stream.of(arrayList1, arrayList2);
+
+        ArrayList<String> actual1 = Ch2.reduceList_3(stream);
+        assertThat(actual1).containsExactly("String1", "String2", "Strong3", "Strong4");
+    }
+
 }
