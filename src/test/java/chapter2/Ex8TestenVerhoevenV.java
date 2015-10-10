d58b2d9c9a0a42484748e94b0801110152099d3d
rRob Swartenbroekx <rob.swartenbroekx@gmail.com>
2015-10-10 09:37:06 +0200

    Chapter 2: ex8: added test, that my solutions can't handle for now (first stream is infinite, second stream isn't).

--- src/test/java/chapter2/Ex8TestenVerhoevenV.java
+++ src/test/java/chapter2/Ex8TestenVerhoevenV.java
@@ -1,6 +1,7 @@
 package chapter2;
 
 import org.assertj.core.api.Assertions;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.util.stream.Stream;
@@ -26,5 +27,18 @@ public class Ex8TestenVerhoevenV {
         Stream<String> zipped = Ex8.zipAlsoCompatibleWithInfiniteStreams(s1, s2);
 
         Assertions.assertThat(zipped.limit(6).toArray()).containsExactly("A", "Z", "A", "Z", "A", "Z");
+    }
+
+    //Added extra scenario myself
+    //For solution see: https://github.com/verhoevenv/Java8ForTheReallyImpatient/blob/master/src/main/java/chapter2/Ex8.java
+    @Ignore
+    @Test
+    public void testZip_OneinfiniteStreams() throws Exception {
+        Stream<String> s1 = Stream.generate(() -> "A");
+        Stream<String> s2 = Stream.of("Z", "X");
+
+        Stream<String> zipped = Ex8.zipAlsoCompatibleWithInfiniteStreams(s1, s2);
+
+        Assertions.assertThat(zipped.toArray()).containsExactly("A", "Z", "A", "X");
     }
 }
